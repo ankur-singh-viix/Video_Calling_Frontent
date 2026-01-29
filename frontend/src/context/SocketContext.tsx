8bc0baccd7b49a5086466ff619528835fbf84070
Ankur Singh <ankurdakshprajapati@example.com>
2026-01-30 01:35:15 +0530

    Room creation logic with user joining the room and there record of joining being stored added

--- frontend/src/context/SocketContext.tsx
+++ frontend/src/context/SocketContext.tsx
@@ -1,6 +1,9 @@
 import SocketIoClient from "socket.io-client";
-import { createContext , useEffect } from "react";
+import { createContext , useEffect , useState} from "react";
 import { useNavigate } from "react-router-dom";
+import Peer from "peerjs";
+import { v4 as UUIDv4 } from "uuid";
+
 
 const ws_Server = "http://localhost:5000";
 
@@ -19,21 +22,34 @@ interface Props {
 export const SocketProvider: React.FC<Props> = ({ children }) => {
 
   const navigate = useNavigate(); // handle navigation with programatinglly 
+  // state varible to store user id
+  const [user, setUser] = useState<Peer>();
+
+  const fetchParticipants = ({roomId , participants} : {roomId : string , participants : string[]}) => {
+    console.log("Participants in room", roomId , "are", participants);
+  }
+
   useEffect(() => {
 
-    const enterRoom = ({roomId} : {roomId : string}) => {
+      const userId = UUIDv4();
+      const newPeer = new Peer(userId);
+      setUser(newPeer);
 
-      navigate(`/room/${roomId}`);
+      
+      const enterRoom = ({roomId} : {roomId : string}) => {
 
-    }
+          navigate(`/room/${roomId}`);
+
+      }
 
     socket.on("room-created", enterRoom);
+    socket.on("get-users", fetchParticipants);
 
   }, []);
 
 
   return (
-    <SocketContext.Provider value={{ socket }}>
+    <SocketContext.Provider value={{ socket , user  }}>
       {children}
     </SocketContext.Provider>
   );
